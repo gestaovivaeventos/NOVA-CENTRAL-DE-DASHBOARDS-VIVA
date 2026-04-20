99db5d101c45e4f5585830ac1b0448c24d816dfd
G. Braz <gestao@vivaeventos.com.br>
2026-04-20 17:20:52 -0300

    feat: ajustar estilos do Shell e Sidebar para melhor controle de layout e responsividade

--- src/modules/central/components/Sidebar.tsx
+++ src/modules/central/components/Sidebar.tsx
@@ -853,7 +853,7 @@ export function Sidebar({ isOpen, onClose }: SidebarProps) {
         className={`
           fixed lg:relative inset-y-0 left-0 z-50
           flex flex-col
-          w-64 h-screen lg:h-auto
+          w-64 h-screen lg:h-full
           transform transition-transform duration-300 ease-in-out
           ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
         `}
